71a3ae80bcf75f91f282961a5df5c02220580b3c
Emily Zhang <zhangemily0820@gmail.com>
2025-03-16 19:31:00 -0400

    Initial code to collect cows, along with background decorations

--- example_map.py
+++ example_map.py
@@ -19,27 +19,133 @@ class ScorePressurePlate(PressurePlate):
         player.set_state("score", player.get_state("score") + 1)
 
         return messages
+    
+class Tree(MapObject): 
+    def __init__(self, image_name: str = "tree_heart"):
+        super().__init__(f"tile/background/{image_name}", passable=True)
+
+class Cow(PressurePlate):
+    def __init__(self, image_name='cow'):
+        super().__init__(image_name)
+        
+    def player_entered(self, player) -> list[Message]:
+        room = player.get_current_room()
+        room.remove_from_grid(self, self.get_position())
+        return []
 
 class ExampleHouse(Map):
     def __init__(self) -> None:
         super().__init__(
             name="Test House",
             description="Welcome",
-            size=(15, 15),
+            size=(50, 50),
             entry_point=Coord(14, 7),
-            background_tile_image='cobblestone',
+            background_tile_image='grass',
         )
     
     def get_objects(self) -> list[tuple[MapObject, Coord]]:
         objects: list[tuple[MapObject, Coord]] = []
 
+        #add tree (temporary, make it better later)
+        tree = Tree() 
+        objects.append((tree, Coord(0,2)))
+
+        tree = Tree() 
+        objects.append((tree, Coord(0,4)))
+
+        tree = Tree() 
+        objects.append((tree, Coord(0,6)))
+
+        tree = Tree() 
+        objects.append((tree, Coord(0,8)))
+
+        tree = Tree() 
+        objects.append((tree, Coord(0,10)))
+
+        tree = Tree() 
+        objects.append((tree, Coord(0,12)))
+
+        tree = Tree() 
+        objects.append((tree, Coord(0,14)))
+
+        tree = Tree() 
+        objects.append((tree, Coord(0,16)))
+
+        tree = Tree() 
+        objects.append((tree, Coord(0,18)))
+
+        tree = Tree() 
+        objects.append((tree, Coord(2,18)))
+
+        tree = Tree() 
+        objects.append((tree, Coord(4,18)))
+
+        tree = Tree() 
+        objects.append((tree, Coord(6,18)))
+
+        tree = Tree() 
+        objects.append((tree, Coord(8,18)))
+
+        tree = Tree() 
+        objects.append((tree, Coord(10,18)))
+
+        tree = Tree() 
+        objects.append((tree, Coord(12,18)))
+
+        tree = Tree() 
+        objects.append((tree, Coord(2,2)))
+
+        tree = Tree() 
+        objects.append((tree, Coord(4,2)))
+
+        tree = Tree() 
+        objects.append((tree, Coord(6,2)))
+
+        tree = Tree() 
+        objects.append((tree, Coord(8,2)))
+
+        tree = Tree() 
+        objects.append((tree, Coord(10,2)))
+
+        tree = Tree() 
+        objects.append((tree, Coord(12,2)))
+
+        tree = Tree() 
+        objects.append((tree, Coord(12,4)))
+
+        tree = Tree() 
+        objects.append((tree, Coord(12,6)))
+
+        tree = Tree() 
+        objects.append((tree, Coord(12,8)))
+
+        tree = Tree() 
+        objects.append((tree, Coord(12,12)))
+
+        tree = Tree() 
+        objects.append((tree, Coord(12,14)))
+
+        tree = Tree() 
+        objects.append((tree, Coord(12,16)))
+
+        tree = Tree() 
+        objects.append((tree, Coord(12,18)))
+
+
+        #add cows 
+        cow = Cow()
+        objects.append((cow, Coord(5, 5)))
+
+        cow = Cow()
+        objects.append((cow, Coord(8, 7)))
+
         # add a door
         door = Door('int_entrance', linked_room="Trottier Town")
-        objects.append((door, Coord(14, 7)))
+        objects.append((door, Coord(14, 10)))
 
         # add a pressure plate
-        pressure_plate = ScorePressurePlate()
-        objects.append((pressure_plate, Coord(13, 7)))
+        # pressure_plate = ScorePressurePlate()
+        # objects.append((pressure_plate, Coord(13, 7)))
 
         return objects
 
