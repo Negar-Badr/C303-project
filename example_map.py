b42510af7278f0674427610f5e64b468336d2329
Emily Zhang <zhangemily0820@gmail.com>
2025-04-07 01:16:32 -0400

    fixed a menu option bug. its a dummy function to help avoid the bug

--- example_map.py
+++ example_map.py
@@ -74,6 +74,9 @@ class EntranceMenuPressurePlate(PressurePlate):
         room.remove_from_grid(self, self.get_position()) 
         command = ShowIntroCommand(self)
         return command.execute(player)
+    def select_option(self, player, option):
+        # Do nothing, or close the menu
+        return []
 
 # -------------------------------------- OUR HOUSE -----------------------------------------------------------------
 class ExampleHouse(Map):
